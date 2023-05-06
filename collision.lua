018b82cacb326687dc6d98cea889cb1eeba8eef1
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2023-05-05 19:57:36 -0600

    Create collision.lua

---
+++ collision.lua
@@ -0,0 +1,21 @@
+--collision function, takes in two tables with x, y, colx (hitbox x), coly (hitbox y)
+
+function col(a,b)
+
+ local a_left=a.x
+ local a_top=a.y
+ local a_right=a.x+a.colw-1
+ local a_bottom=a.y+a.colh-1
+ 
+ local b_left=b.x
+ local b_top=b.y
+ local b_right=b.x+b.colw-1
+ local b_bottom=b.y+b.colh-1
+
+ if a_top>b_bottom then return false end
+ if b_top>a_bottom then return false end
+ if a_left>b_right then return false end
+ if b_left>a_right then return false end
+ 
+ return true
+end
